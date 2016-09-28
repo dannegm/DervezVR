96f14e1bd3ce495fb303f9807c1bda4a771833f5
dannegm <im@danielgarcia.me>
2016-09-27 22:07:44 -0500

    Normalize

---
+++ Assets/CameraVR/Script/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CameraController : MonoBehaviour {
+
+	public Camera LeftEye;
+	public Camera RightEye;
+	public GUIStyle OculusStyle;
+
+	public float FieldOfView;
+
+	private float width;
+	private float height;
+	private Rect guirect;
+
+	void Start () {
+	}
+
+	void Update () {
+		LeftEye.fieldOfView = FieldOfView;
+		RightEye.fieldOfView = FieldOfView;
+	}
+
+	void OnGUI () {
+		width = Screen.width;
+		height = Screen.height;
+		guirect = new Rect (0, 0, width, height);
+		GUI.Box (guirect, "", OculusStyle);
+	}
+}
---
+++ Assets/Scripts/LookToSound.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookToSound : MonoBehaviour {
+
+	private bool canPlayAudio = false;
+
+	void OnTriggerEnter(Collider other) {
+		if (canPlayAudio) {
+			canPlayAudio = false;
+			audio.Play();
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		canPlayAudio = true;
+	}
+}
